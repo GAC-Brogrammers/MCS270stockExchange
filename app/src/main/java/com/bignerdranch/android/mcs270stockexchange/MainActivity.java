2e6ff11221dc353315a5be9789cfa727e8e32e4a
Tanner <tmartin2@gustavus.edugit config --global user.email tmartin2@gustavus.edu>
2016-04-19 17:34:52 -0500

    Should have all xml's and menu items

--- app/src/main/java/com/bignerdranch/android/mcs270stockexchange/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/mcs270stockexchange/MainActivity.java
@@ -3,16 +3,13 @@ package com.bignerdranch.android.mcs270stockexchange;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import yahoofinance.Stock;
-import yahoofinance.YahooFinance;
-
 public class MainActivity extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.fragment_stock);
 
 
 
